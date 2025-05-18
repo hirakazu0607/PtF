0e3475625ee5dc714851546a014dcc1958047b6f
hiranokazuma <surakazu0607@outlook.jp>
2025-05-18 16:03:55 +0900

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container fluid>
+        <Navbar />
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from "../components/problemComponents/Navbar.vue";
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+  components: { Navbar },
+};
+</script>
---
+++ pages/list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-container>
+      <v-row
+        ><v-col lg="6" md="12"
+          ><ListProblems
+            @onselectRow="changeRow"
+            :selectedRow="selectedProblem"
+        /></v-col>
+        <v-col v-show="!!selectedProblem" lg="6" md="12"
+          ><PDFView :selectedRow="selectedProblem" /></v-col
+      ></v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import ListProblems from "../components/problemComponents/ListProblems.vue";
+import PDFView from "../components/problemComponents/PDFView.vue";
+export default {
+  name: "IndexPage",
+  components: { ListProblems, PDFView },
+  data() {
+    return {
+      selectedProblem: null,
+    };
+  },
+  methods: {
+    changeRow(newRow) {
+      console.log("here is list: ", newRow);
+      this.selectedProblem = newRow;
+    },
+  },
+};
+</script>
